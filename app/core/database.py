1b356b7146bb04a79b7cccde42bb9edb93819944
Markus <abramianmedina@example.com>
2025-10-08 20:33:19 +0000

    alembic configured with sqlmodel

--- app/core/database.py
+++ app/core/database.py
@@ -1,17 +1,16 @@
 from sqlmodel import SQLModel, create_engine, Session
 from app.core.config import settings
 
-# ⚙️ Crear el motor de conexión
 engine = create_engine(
     settings.DATABASE_URL,
-    echo=False,  # Cambia a True si quieres ver los SQL logs
-    pool_pre_ping=True,  # Revisa que las conexiones sigan vivas
+    echo=False,  # SQL Logs
+    pool_pre_ping=True, 
 )
 
 
 def init_db():
     SQLModel.metadata.create_all(engine)
-    print("✅ Tablas creadas o verificadas correctamente.")
+    print("Tablas creadas o verificadas correctamente.")
 
 
 def get_session():
--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from app.core.database import init_db
 from app.routers.drive import router as drive_router
 from app.routers.rag import router as rag_router
 
@@ -6,6 +8,14 @@ app = FastAPI(title="Chatbot RAG Backend")
 
 app.include_router(drive_router)
 app.include_router(rag_router)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Iniciando aplicación Crecenia Chatbot...")
+    init_db()  
+    yield
+    print("Cerrando aplicación Crecenia Chatbot...")
 
 
 @app.get("/")
